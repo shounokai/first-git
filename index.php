75fe40dfd4b662440ad11e4a6e27ec30971d8151
akadmia.tec <dokodemo1293@gmail.com>
2020-03-19 11:56:59 +0000

    sentents plus

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="ja">
+    <head>
+        <meta charset="utf-8">
+        <title>GIT HUb</title>
+    </head>
+    <body>
+        <h2>初コミット</h2>
+        <p>これから初めてのコミットをする</p>
+    </body>
+</html>
